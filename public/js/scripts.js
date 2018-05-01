da9932354a93d899f5c7a78b3d432ecf796224b8
Sabrina Kennedy <skenne21@gmail.com>
2018-04-30 20:39:23 -0600

    Add adjustment to layout and styles of palettes

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,5 +1,4 @@
-const randomColorGenerator = (event) => {
-  event.preventDefault();
+const randomColorGenerator = () => {
   for (let i = 0; i < 5; i++) {
     const color =  '#' + Math.floor(Math.random()*16777215).toString(16);
     const palletes = $('article').get();
@@ -11,11 +10,16 @@ const randomColorGenerator = (event) => {
   }
 }
 
+
 const toggleLock = (event) => {
   const pallete = event.target.closest('article');
+  const button = event.target.closest('button');
   $(pallete).toggleClass('lock');
+  $(button).toggleClass('locked');
+  console.log(pallete)
 }
 
 $('.palette_generator').on('click', randomColorGenerator);
-$('.lock').on('click', toggleLock);
+$('.lock_btn').on('click', toggleLock);
 
+$('document').ready(randomColorGenerator);
