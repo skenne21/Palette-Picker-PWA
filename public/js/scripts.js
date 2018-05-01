3356529e2b528dc3316999f4a08116939e75f59b
Sabrina Kennedy <skenne21@gmail.com>
2018-04-30 19:37:05 -0600

    Add lock functionalilty

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,13 +1,21 @@
-const randomColorGenerator = () => {
+const randomColorGenerator = (event) => {
   event.preventDefault();
-  const colors = [];
   for (let i = 0; i < 5; i++) {
     const color =  '#' + Math.floor(Math.random()*16777215).toString(16);
     const palletes = $('article').get();
-    $(palletes[i]).css('background-color', color)
+    const hexText = $('h2').get();
+    if(!$(palletes[i]).hasClass('lock')) {
+      $(palletes[i]).css('background-color', color);
+      $(hexText[i]).text(color);
+    }
   }
 }
 
-$('.palette_generator').on('click', randomColorGenerator);
+const toggleLock = (event) => {
+  const pallete = event.target.closest('article');
+  $(pallete).toggleClass('lock');
+}
 
+$('.palette_generator').on('click', randomColorGenerator);
+$('.lock').on('click', toggleLock);
 
